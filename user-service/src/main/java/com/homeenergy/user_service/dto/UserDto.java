bf484e1bde5514025dff513db14ea7c066c1608e
Manish Kumar <mmm.manish163@gmail.com>
2026-04-10 12:53:29 +0530

    user service implemented

--- user-service/src/main/java/com/homeenergy/user_service/dto/UserDto.java
+++ user-service/src/main/java/com/homeenergy/user_service/dto/UserDto.java
@@ -1,9 +1,13 @@
 package com.homeenergy.user_service.dto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private Long id;
     private String name;
